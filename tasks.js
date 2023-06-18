ce28d2686fab748cca0e4eeefe03d07315959abe
Mathabela Victor <86621079+VectorM2@users.noreply.github.com>
2023-06-18 18:19:46 +0200

    second commit // this is functional

--- tasks.js
+++ tasks.js
@@ -1,10 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import {
   getAuth,
-  createUserWithEmailAndPassword,signInWithEmailAndPassword
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-import { getDatabase, push } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
-
+import { getDatabase, push, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyApdCfhPlJOMQ0uZpX8O8PAiUSHpWR595I",
@@ -19,28 +18,120 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
-// Reference
-const tasksRef = ref(database, "tasks");
 
-const addTask = document.getElementById("click");
-addTask.addEventListener("click", function() {
-  var user = firebase.auth().currentUser;
-  const taskInput = document.getElementById("taskInput").value;
-
+//fetch current user.
+onAuthStateChanged(auth, (user) => {
   if (user) {
-    const newTask = {
-      task: taskInput,
-      uid: user.uid
-    };
+    const uid = user.uid;
+    const displayName = user.displayName;
+    const email = user.email;
+    const profileEmail = document.getElementById("profileEmail");
+    profileEmail.innerHTML = email;
+    console.log(uid, email, displayName);
+   
+    fetchData()
 
-    push(tasksRef, newTask)
-      .then(() => {
-        console.log("Task added successfully!");
-      })
-      .catch((error) => {
-        console.error("Error adding task:", error);
-      });
+
   } else {
-    console.log("No user is currently signed in.");
+    console.log(8);
+  }
+
+
+});
+
+// ...
+
+const addTask = document.getElementById("addTask");
+addTask.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const taskInput = document.getElementById("taskInput");
+  const taskValue = taskInput.value.trim(); // Remove leading and trailing whitespace
+
+  if (taskValue !== "") {
+    const userId = auth.currentUser.uid;
+    const userTasksRef = ref(database, 'users/' + userId + '/tasks');
+    push(userTasksRef, taskValue)
+      .then(function () {
+        console.log("Task added successfully!");
+        taskInput.value = ""; // Clear the input field
+      })
+      .catch(function (error) {
+        console.error("Error adding task: ", error);
+      });
   }
 });
+
+// ...
+
+
+
+function fetchData() {
+  const userId = auth.currentUser.uid;
+  const userTasksRef = ref(database, 'users/' + userId + '/tasks');
+  onValue(userTasksRef, function (snapshot) {
+    const tasksContainer = document.getElementById("tasks");
+    tasksContainer.innerHTML = ""; // Clear existing tasks
+
+    const taskList = Object.values(snapshot.val() || {}); // Retrieve task list or an empty object
+
+    if (taskList.length === 0) {
+      const li = document.createElement("li");
+      li.innerText = "No tasks found.";
+      tasksContainer.appendChild(li);
+    } else {
+      taskList.forEach(task => {
+        const li = document.createElement("li");
+        li.innerText = task;
+        tasksContainer.appendChild(li);
+      });
+    }
+  });
+}
+
+// ...
+
+
+
+
+//signout
+const logOut = document.getElementById("signOut");
+logOut.addEventListener("click", function(){
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
+})
+
+// Function to delete all tasks in the database
+function deleteAllTasks() {
+  const user = auth.currentUser;
+  if (user) {
+    const userId = user.uid;
+    const userTasksRef = ref(database, 'users/' + userId + '/tasks');
+    set(userTasksRef, null)
+      .then(function () {
+        console.log("All tasks deleted successfully!");
+      })
+      .catch(function (error) {
+        console.error("Error deleting tasks: ", error);
+      });
+  }
+}
+
+const erase = document.getElementById("delete");
+erase.addEventListener("click",function(){
+  const user = auth.currentUser;
+  if (user) {
+    const userId = user.uid;
+    const userTasksRef = ref(database, 'users/' + userId + '/tasks');
+    set(userTasksRef, null)
+      .then(function () {
+        console.log("All tasks deleted successfully!");
+      })
+      .catch(function (error) {
+        console.error("Error deleting tasks: ", error);
+      });
+  }
+})
